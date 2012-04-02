14da1a869a3eef55448977336614faf8b6aeb0cf
Devin Blais <devinblais+git@gmail.com>
2012-04-01 17:36:31 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LMFAOParty::Application.config.secret_token = '32c2791af6407a81afb4580c6c0571fdf3c1c6f2b5dbf03cd2092ab4f7d4ecf52a41a426efc84792e54ae515ea3ed7ecf036d67ebfd3d043197d46812ca53639'
